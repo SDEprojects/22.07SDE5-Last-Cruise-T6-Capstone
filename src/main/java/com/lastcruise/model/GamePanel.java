beb098bedfac6fea048216b44ed5dc1c5c8b177d
akwarkoh <angelakwarko@gmail.com>
2022-11-02 10:11:05 -0700

    added rope item to resources folder, AB#1177

--- src/main/java/com/lastcruise/model/GamePanel.java
+++ src/main/java/com/lastcruise/model/GamePanel.java
@@ -29,8 +29,9 @@ public class GamePanel extends JPanel implements Runnable {
   private KeyHandler keyHandler = new KeyHandler();
   private Thread gameThread;
   private Player player = new Player();
-  private TileManager tileManager = new TileManager( maxScreenCol, maxScreenRow);
-  private Collision collision = new Collision(tileSize, tileManager.getMapTileIndex(), tileManager.getTile());
+  private TileManager tileManager = new TileManager(maxScreenCol, maxScreenRow);
+  private Collision collision = new Collision(tileSize, tileManager.getMapTileIndex(),
+      tileManager.getTile());
   private Game game;
   private Inventory inventory;
   private GameMap gameMap;
@@ -59,7 +60,7 @@ public class GamePanel extends JPanel implements Runnable {
 
   @Override
   public void run() {
-    double drawInterval = 1000000000/FPS;
+    double drawInterval = 1000000000 / FPS;
     double delta = 0;
     long lastTime = System.nanoTime();
     long currentTime;
@@ -80,9 +81,11 @@ public class GamePanel extends JPanel implements Runnable {
     if (game.getState() == State.SLEEP) {
       player.sleep();
     }
-    if (keyHandler.isUpPressed() || keyHandler.isDownPressed() || keyHandler.isLeftPressed() || keyHandler.isRightPressed()){
+    if (keyHandler.isUpPressed() || keyHandler.isDownPressed() || keyHandler.isLeftPressed()
+        || keyHandler.isRightPressed()) {
       // update the player's direction when someone presses W, A, S, or D
-      player.updateDirection(keyHandler.isUpPressed(), keyHandler.isDownPressed(), keyHandler.isLeftPressed(), keyHandler.isRightPressed());
+      player.updateDirection(keyHandler.isUpPressed(), keyHandler.isDownPressed(),
+          keyHandler.isLeftPressed(), keyHandler.isRightPressed());
       // check if player collides with certain tiles
       checkForMapCollision();
       // update the position if the player can continue without collision
@@ -110,7 +113,7 @@ public class GamePanel extends JPanel implements Runnable {
     Graphics2D g2 = (Graphics2D) g;
     //if (gameState == title){
     //  titleScreen()}
-    if (game.getState().equals(State.TITLE)){
+    if (game.getState().equals(State.TITLE)) {
       view.titleScreen(g2, tileSize, screenWidth);
 
     } else {
@@ -131,101 +134,107 @@ public class GamePanel extends JPanel implements Runnable {
       // draw player
       player.draw(g2, tileSize);
 
-    // draw stamina bar
-    gameUI.drawPlayerStamina(g2, player.getStamina());
+      // draw stamina bar
+      gameUI.drawPlayerStamina(g2, player.getStamina());
 
-    // draw the player inventory
-    if(game.getState() == State.INVENTORY) {
-      gameUI.drawInventory(this, g2, player.getInventory());
-    }
-
-    if (game.getState().equals(State.HELP)) {
-      gameUI.drawHelpMenu(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2);
-    }
-    if (game.getState().equals(State.SLEEP)) {
-      if (player.getStamina() < 100){
-        gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2, new String[]{"Sleepy time...zzzZZZzzZZzz "});
-      } else {
-        gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2, new String[]{"You have full stamina. WAKE UP!"});
+      // draw the player inventory
+      if (game.getState() == State.INVENTORY) {
+        gameUI.drawInventory(this, g2, player.getInventory());
       }
-    }
-    g2.dispose();
-  }
 
-  public void setupGame() {
-    game.setState(State.TITLE);
-    // set inventory to the inventory of the current location
-    if (game.getCurrentLocationInventory() != null) {
-      this.inventory = game.getCurrentLocationInventory();
-    }
-    // create instances of all locations
-    if (game.getGameMap() != null) {
-      this.gameMap = game.getGameMap();
-    }
-    // set the first location (beach)
-    gameMap.setCurrentLocation(gameMap.getLocations().get("BEACH"));
-    // load the initial map
-    tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
-//    System.out.println(gameMap.getCurrentLocation().getNorth().get("x"));
-  }
-
-  private void checkForMapCollision() {
-    // the bounds of player's collision area
-    int topRow = (player.getY() + player.getSolidArea().y - player.getSpeed()) / tileSize;
-    int bottomRow = (player.getY() + player.getSolidArea().y + player.getSolidArea().height + player.getSpeed()) / tileSize;
-    int leftCol = (player.getX() + player.getSolidArea().x - player.getSpeed()) / tileSize;
-    int rightCol = (player.getX() + player.getSolidArea().x + player.getSolidArea().width + player.getSpeed()) / tileSize;
-
-    // check if the next move will collide with a certain tile
-    // out of bounds exception stops player from going off map
-    try {
-      player.setCollisionOn(collision.checkTile(topRow, bottomRow, leftCol, rightCol, player.getDirection()));
-    } catch (ArrayIndexOutOfBoundsException e) {
-      player.setCollisionOn(true);
+      if (game.getState().equals(State.HELP)) {
+        gameUI.drawHelpMenu(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2);
+      }
+      if (game.getState().equals(State.SLEEP)) {
+        if (player.getStamina() < 100) {
+          gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
+              new String[]{"Sleepy time...zzzZZZzzZZzz "});
+        } else {
+          gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
+              new String[]{"You have full stamina. WAKE UP!"});
+        }
+      }
+      g2.dispose();
     }
   }
 
-  // checks if the map needs to change and places player in correct map and position
-  private void updateMap() {
-    int tileX = player.getX() / tileSize;
-    int tileY = player.getY() / tileSize;
-    Location current = gameMap.getCurrentLocation();
-    // checks if the tile is a transition tile and returns current map
-    String newMap = current.checkForMapTransition(tileX, tileY, player.getDirection());
-    if (!current.getName().equals(newMap)){
-      // change current location to new location
-      gameMap.setCurrentLocation(gameMap.getLocations().get(newMap));
-      // places the player at the entrance for the new map
-      int[] entrance = gameMap.getCurrentLocation().getEntranceCoordinates(player.getDirection());
-      player.setX(entrance[0] * tileSize);
-      player.setY(entrance[1] * tileSize);
-      // load the new map
+    public void setupGame () {
+      //game.setState(State.TITLE);
+      // set inventory to the inventory of the current location
+      if (game.getCurrentLocationInventory() != null) {
+        this.inventory = game.getCurrentLocationInventory();
+      }
+      // create instances of all locations
+      if (game.getGameMap() != null) {
+        this.gameMap = game.getGameMap();
+      }
+      // set the first location (beach)
+      gameMap.setCurrentLocation(gameMap.getLocations().get("BEACH"));
+      // load the initial map
       tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
+//    System.out.println(gameMap.getCurrentLocation().getNorth().get("x"));
     }
-  }
 
+    private void checkForMapCollision () {
+      // the bounds of player's collision area
+      int topRow = (player.getY() + player.getSolidArea().y - player.getSpeed()) / tileSize;
+      int bottomRow = (player.getY() + player.getSolidArea().y + player.getSolidArea().height
+          + player.getSpeed()) / tileSize;
+      int leftCol = (player.getX() + player.getSolidArea().x - player.getSpeed()) / tileSize;
+      int rightCol = (player.getX() + player.getSolidArea().x + player.getSolidArea().width
+          + player.getSpeed()) / tileSize;
 
-  public void pickupItem(String itemName) {
-    if(!itemName.equals("")) {
+      // check if the next move will collide with a certain tile
+      // out of bounds exception stops player from going off map
       try {
-
-        player.setStamina(player.getStamina() - 10);
-
-        // remove the item from the location inventory and add it to the player inventory
-        game.transferItemFromTo(inventory, player.getInventory(), itemName);
-
-      } catch (InventoryEmptyException e){
-        System.out.println("Item " + itemName + " is not in inventory!");
-      } catch (NoEnoughStaminaException e) {
-        //throw new RuntimeException(e);
-        System.out.println("Not enough stamina to pickup item!");
+        player.setCollisionOn(
+            collision.checkTile(topRow, bottomRow, leftCol, rightCol, player.getDirection()));
+      } catch (ArrayIndexOutOfBoundsException e) {
+        player.setCollisionOn(true);
       }
     }
+
+    // checks if the map needs to change and places player in correct map and position
+    private void updateMap () {
+      int tileX = player.getX() / tileSize;
+      int tileY = player.getY() / tileSize;
+      Location current = gameMap.getCurrentLocation();
+      // checks if the tile is a transition tile and returns current map
+      String newMap = current.checkForMapTransition(tileX, tileY, player.getDirection());
+      if (!current.getName().equals(newMap)) {
+        // change current location to new location
+        gameMap.setCurrentLocation(gameMap.getLocations().get(newMap));
+        // places the player at the entrance for the new map
+        int[] entrance = gameMap.getCurrentLocation().getEntranceCoordinates(player.getDirection());
+        player.setX(entrance[0] * tileSize);
+        player.setY(entrance[1] * tileSize);
+        // load the new map
+        tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
+      }
+    }
+
+    public void pickupItem (String itemName){
+      if (!itemName.equals("")) {
+        try {
+
+          player.setStamina(player.getStamina() - 10);
+
+          // remove the item from the location inventory and add it to the player inventory
+          game.transferItemFromTo(inventory, player.getInventory(), itemName);
+
+        } catch (InventoryEmptyException e) {
+          System.out.println("Item " + itemName + " is not in inventory!");
+        } catch (NoEnoughStaminaException e) {
+          //throw new RuntimeException(e);
+          System.out.println("Not enough stamina to pickup item!");
+        }
+      }
+    }
+
+    public int getTileSize () {
+      return tileSize;
+    }
+
   }
 
-  public int getTileSize() {
-    return tileSize;
-  }
-
-}
 
