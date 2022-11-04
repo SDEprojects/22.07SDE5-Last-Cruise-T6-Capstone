c5ca1764d38d1ce031b37a17563df57dcd7f5a48
Kelly Chhe <102538779+kellychhe@users.noreply.github.com>
2022-11-04 16:42:31 -0400

    refactor methods, AB#1371

--- src/main/java/com/lastcruise/controller/Game.java
+++ src/main/java/com/lastcruise/controller/Game.java
@@ -11,33 +11,30 @@ import com.lastcruise.model.State;
 import com.lastcruise.model.entity.Player;
 import com.lastcruise.model.entity.Player.ItemNotEdibleException;
 import com.lastcruise.model.entity.Player.NoEnoughStaminaException;
+import com.lastcruise.view.GamePanel;
+import com.lastcruise.view.audio.SoundEffect;
+import java.util.ConcurrentModificationException;
 import java.util.Set;
 
 public class Game {
 
     private Player player;
-
     private GameMap gameMap;
-
     private State state;
-
+    private Inventory inventory;
+    private GamePanel gamePanel;
+    private SoundEffect soundEffect;
     private static final String STARTING_LOCATION = "BEACH";
-
-    public Game() {
-
-    }
-
     public Game(Player player) {
         this.gameMap = new GameMap();
-        this.state = State.PLAY;
         gameMap.setStartLocation(gameMap.getLocations().get(STARTING_LOCATION));
+
+        this.state = State.TITLE;
+        this.inventory = getCurrentLocationInventory();
+//        this.gamePanel = new GamePanel();
+        this.soundEffect = new SoundEffect();
     }
 
-    public Game(String name){
-        this.gameMap = new GameMap();
-        this.state = State.PLAY;
-        gameMap.setStartLocation(gameMap.getLocations().get(STARTING_LOCATION));
-    }
     public Player getPlayer() {
         return player;
     }
@@ -88,6 +85,26 @@ public class Game {
         return String.valueOf(player.getStamina());
     }
 
+    public boolean updateMap(int tileSize) {
+        int tileX = player.getX() / tileSize;
+        int tileY = player.getY() / tileSize;
+        Location current = gameMap.getCurrentLocation();
+        // checks if the tile is a transition tile and returns current map
+        String newMap = current.checkForMapTransition(tileX, tileY, player.getDirection());
+        if (!current.getName().equals(newMap)) {
+            // change current location to new location
+            gameMap.setCurrentLocation(gameMap.getLocations().get(newMap));
+            // places the player at the entrance for the new map
+            int[] entrance = gameMap.getCurrentLocation().getEntranceCoordinates(player.getDirection());
+            player.setX(entrance[0] * tileSize);
+            player.setY(entrance[1] * tileSize);
+            // load the new map
+            soundEffect.loadAndPlayFx("run");
+            this.inventory = getCurrentLocationInventory();
+            return true;
+        }
+        return false;
+    }
 
     public void transferItemFromTo(Inventory from, Inventory to, String itemName)
         throws InventoryEmptyException, NoEnoughStaminaException {
@@ -103,7 +120,55 @@ public class Game {
             to.add(itemName, removed);
         }
     }
-
+    public void pickupItem (String itemName){
+        if (!itemName.equals("") && (player.getInventory().getInventory().size() < 8)) {
+            System.out.println("Num of items: " + player.getInventory().getInventory().size());
+            try {
+                player.reduceStaminaPickUp();
+                // remove the item from the location inventory and add it to the player inventory
+                System.out.println(inventory.getInventory().toString());
+                transferItemFromTo(inventory, player.getInventory(), itemName);
+                soundEffect.loadAndPlayFx("pickup");
+            } catch (InventoryEmptyException e) {
+                System.out.println("Item " + itemName + " is not in inventory!");
+            } catch (NoEnoughStaminaException e) {
+                //throw new RuntimeException(e);
+                System.out.println("Not enough stamina to pickup item!");
+            }
+        }
+    }
+    public void useItem(int index) {
+        Item foundItem = null;
+        for (Item item : getPlayerInventory().getInventory().values()) {
+            if (item.getIndex() == index) {
+                foundItem = item;
+                System.out.println("Index: " + index + " Name: " + item.getName());
+            }
+        }
+        if (foundItem != null) {
+            String action = foundItem.checkEatOrDrop();
+            if (action.equals("eat")) {
+                try {
+                    eatItem(foundItem.getName());
+                    soundEffect.loadAndPlayFx("eat");
+                } catch (InventoryEmptyException | ItemNotEdibleException |
+                         ConcurrentModificationException e) {
+                    //System.out.println(e);
+                }
+            } else if (action.equals("drop")) {
+                try {
+                    foundItem.updateItemLocation(player.getX(), player.getY());
+                    transferItemFromTo(getPlayerInventory(), getCurrentLocationInventory(),
+                        foundItem.getName());
+                    soundEffect.loadAndPlayFx("drop");
+                } catch (InventoryEmptyException |
+                         ConcurrentModificationException |
+                         NoEnoughStaminaException e) {
+                    //System.out.println(e);
+                }
+            }
+        }
+    }
     public void eatItem(String itemName) throws InventoryEmptyException, ItemNotEdibleException {
         player.consumeItem(itemName);
     }
--- src/main/java/com/lastcruise/view/GamePanel.java
+++ src/main/java/com/lastcruise/view/GamePanel.java
@@ -7,10 +7,7 @@ import com.lastcruise.model.Inventory;
 import com.lastcruise.model.Item;
 import com.lastcruise.model.Location;
 import com.lastcruise.model.State;
-import com.lastcruise.model.Inventory.InventoryEmptyException;
 import com.lastcruise.model.entity.Player;
-import com.lastcruise.model.entity.Player.ItemNotEdibleException;
-import com.lastcruise.model.entity.Player.NoEnoughStaminaException;
 import com.lastcruise.model.tile.TileManager;
 import com.lastcruise.view.audio.AllSounds;
 import com.lastcruise.view.audio.Music;
@@ -20,7 +17,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.net.URL;
-import java.util.ConcurrentModificationException;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -47,8 +43,6 @@ public class GamePanel extends JPanel implements Runnable {
   private Game game;
   private Inventory inventory;
   private GameMap gameMap;
-  private View view;
-  private Music music;
   private SoundEffect soundEffect;
   private GameUI gameUI;
 
@@ -60,13 +54,11 @@ public class GamePanel extends JPanel implements Runnable {
     this.setDoubleBuffered(true);
     this.addKeyListener(keyHandler);
     this.setFocusable(true);
-    this.inventory = new Inventory();
     this.game = new Game(player);
-    this.view = new View();
-    this.music = new Music();
+    this.inventory = new Inventory();
     this.soundEffect = new SoundEffect();
     this.gameUI = new GameUI();
-    this.tileManager = new TileManager( maxScreenCol, maxScreenRow);
+    this.tileManager = new TileManager(maxScreenCol, maxScreenRow);
     this.collision = new Collision(tileSize, tileManager.getMapTileIndex(), tileManager.getTile());
     keyHandler.setGame(game);
     keyHandler.setGameUI(gameUI);
@@ -98,70 +90,78 @@ public class GamePanel extends JPanel implements Runnable {
   }
 
   public void update() {
-    if (game.getState() == State.TITLE && keyHandler.isEnterPressed()) {
-      switch (gameUI.getRowSelection()){
-        case 0:
-          game.setState(State.PLAY);
-          break;
-        case 1:
-          game.setState(State.PLAY);
-          System.out.println("load game");
-          break;
-        case 2:
-          System.exit(0);
-          break;
-      }
-      keyHandler.setEnterPressed(false);
-    }
-    if (game.getState() == State.SLEEP) {
-      player.sleep();
-      soundEffect.loadAndPlayFx("sleep");
-    }
-    if (game.getState() == State.INVENTORY){
-      if (keyHandler.isEnterPressed()){
-        dropItem();
-      }
+    switch (game.getState()) {
+      case TITLE:
+        if (keyHandler.isEnterPressed()) {
+          switch (gameUI.getRowSelection()) {
+            case 0:
+              game.setState(State.PLAY);
+              break;
+            case 1:
+              game.setState(State.PLAY);
+              System.out.println("load game");
+              break;
+            case 2:
+              System.exit(0);
+              break;
+          }
+          keyHandler.setEnterPressed(false);
+        }
+        break;
+      case SLEEP:
+        player.sleep();
+        soundEffect.loadAndPlayFx("sleep");
+        break;
+      case INVENTORY:
+        if (keyHandler.isEnterPressed()) {
+          game.useItem(gameUI.getItemIndex());
+          keyHandler.setEnterPressed(false);
+        }
+        break;
+      case WIN:
+        if (keyHandler.isEnterPressed()) {
+          keyHandler.setEnterPressed(false);
+          if (gameUI.getWinGameBoxPosition() == 0) {
+            System.out.println("Start new game selected!");
+            newGame();
+            setupGame();
+            playBackgroundMusic();
+          } else {
+            System.exit(0);
+          }
+        }
+        break;
+      case PLAY:
+        if (keyHandler.isBuildPressed()) {
+          game.craftRaft();
+          soundEffect.loadAndPlayFx("build");
+          keyHandler.setBuildPressed(false);
+        }
+        if (keyHandler.isUpPressed() || keyHandler.isDownPressed() || keyHandler.isLeftPressed()
+            || keyHandler.isRightPressed()) {
+          soundEffect.loadAndPlayFx("footsteps");
+          // update the player's direction when someone presses W, A, S, or D
+          player.updateDirection(keyHandler.isUpPressed(), keyHandler.isDownPressed(),
+              keyHandler.isLeftPressed(), keyHandler.isRightPressed());
+          // check if player collides with certain tiles
+          checkForMapCollision();
+          // update the position if the player can continue without collision
+          player.updatePosition();
+          // update player stamina status
+          player.updateStamina();
+          // check if player needs to change maps
+          boolean changeMap = game.updateMap(tileSize);
+          if (changeMap){
+            tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
+          }
+
+        }
+        break;
     }
 
-    if (game.getState() == State.WIN) {
-      if (keyHandler.isEnterPressed()) {
-        keyHandler.setEnterPressed(false);
-        if (gameUI.getWinGameBoxPosition() == 0) {
-          System.out.println("Start new game selected!");
-          game.setState(State.TITLE);
-        } else {
-          System.exit(0);
-        }
-      }
-    }
-    if (keyHandler.isBuildPressed()) {
-      game.craftRaft();
-      soundEffect.loadAndPlayFx("build");
-      keyHandler.setBuildPressed(false);
-    }
-    if (keyHandler.isUpPressed() || keyHandler.isDownPressed() || keyHandler.isLeftPressed() || keyHandler.isRightPressed()){
-      soundEffect.loadAndPlayFx("footsteps");
-      // update the player's direction when someone presses W, A, S, or D
-      player.updateDirection(keyHandler.isUpPressed(), keyHandler.isDownPressed(),
-          keyHandler.isLeftPressed(), keyHandler.isRightPressed());
-      // check if player collides with certain tiles
-      checkForMapCollision();
-      // update the position if the player can continue without collision
-      player.updatePosition();
-      // update player stamina status
-      player.updateStamina();
-      // check if player needs to change maps
-      updateMap();
-    }
     // check item collision
     String itemName = collision.checkItem(player, true, inventory);
-    pickupItem(itemName);
-
-//    if(keyHandler.isInventoryState()) {
-//      game.setState(State.INVENTORY);
-//    } else {
-//      game.setState(State.PLAY);
-//    }
+    game.pickupItem(itemName);
   }
 
   // checks if the map needs to change and places player in correct map and position
@@ -169,8 +169,6 @@ public class GamePanel extends JPanel implements Runnable {
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D) g;
-    //if (gameState == title){
-    // titleScreen()}
     if (game.getState().equals(State.TITLE)) {
       gameUI.titleScreen(g2, tileSize, screenWidth);
     } else if (game.getState().equals(State.WIN)) {
@@ -181,7 +179,6 @@ public class GamePanel extends JPanel implements Runnable {
       stopBackgroundMusic();
       gameUI.loseScreen(g2, tileSize, screenWidth);
     } else {
-
       // draw tiles
       tileManager.draw(g2, tileSize);
       // draw items
@@ -201,23 +198,24 @@ public class GamePanel extends JPanel implements Runnable {
       // draw stamina bar
       gameUI.drawPlayerStamina(g2, player.getStamina());
 
-      // draw the player inventory
-      if (game.getState() == State.INVENTORY) {
-        gameUI.drawInventory(this, g2, player.getInventory());
+      switch (game.getState()) {
+        case INVENTORY:
+          // draw the player inventory
+          gameUI.drawInventory(this, g2, player.getInventory());
+          break;
+        case HELP:
+          gameUI.drawHelpMenu(tileSize * 2, tileSize * 7, tileSize * 12, tileSize * 5, g2);
+          break;
+        case SLEEP:
+          if (player.getStamina() < 100) {
+            gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
+                new String[]{"Sleepy time...zzzZZZzzZZzz "});
+          } else {
+            gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
+                new String[]{"You have full stamina. WAKE UP!"});
+          }
       }
 
-      if (game.getState().equals(State.HELP)) {
-        gameUI.drawHelpMenu(tileSize * 2, tileSize * 7, tileSize * 12, tileSize * 5, g2);
-      }
-      if (game.getState().equals(State.SLEEP)) {
-        if (player.getStamina() < 100) {
-          gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
-              new String[]{"Sleepy time...zzzZZZzzZZzz "});
-        } else {
-          gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
-              new String[]{"You have full stamina. WAKE UP!"});
-        }
-      }
       if (player.getStamina() == 0) {
         gameUI.drawStringInSubWindow(tileSize * 2, tileSize * 8, tileSize * 12, tileSize * 4, g2,
             new String[]{"Uh oh...", "You have no more stamina!", "Are you tired?",
@@ -226,120 +224,59 @@ public class GamePanel extends JPanel implements Runnable {
       g2.dispose();
     }
   }
-
-    public void setupGame () {
-      game.setState(State.TITLE);
-      // set inventory to the inventory of the current location
-      if (game.getCurrentLocationInventory() != null) {
-        this.inventory = game.getCurrentLocationInventory();
-      }
-      // create instances of all locations
-      if (game.getGameMap() != null) {
-        this.gameMap = game.getGameMap();
-      }
-      // set the first location (beach)
-      gameMap.setCurrentLocation(gameMap.getLocations().get("BEACH"));
-      // load the initial map
-      tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
+  public void newGame() {
+    player = new Player();
+    game = new Game(player);
+    inventory = new Inventory();
+    gameUI = new GameUI();
+    tileManager = new TileManager(maxScreenCol, maxScreenRow);
+    collision = new Collision(tileSize, tileManager.getMapTileIndex(), tileManager.getTile());
+    keyHandler.setGame(game);
+    keyHandler.setGameUI(gameUI);
+    game.setPlayer(this.player);
+  }
+  public void setupGame() {
+    game.setState(State.TITLE);
+    // set inventory to the inventory of the current location
+    if (game.getCurrentLocationInventory() != null) {
+      this.inventory = game.getCurrentLocationInventory();
+    }
+    // create instances of all locations
+    if (game.getGameMap() != null) {
+      this.gameMap = game.getGameMap();
+    }
+    // set the first location (beach)
+    gameMap.setCurrentLocation(gameMap.getLocations().get("BEACH"));
+    // load the initial map
+    tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
 //    System.out.println(gameMap.getCurrentLocation().getNorth().get("x"));
-    }
+  }
 
-    private void checkForMapCollision () {
-      // the bounds of player's collision area
-      int topRow = (player.getY() + player.getSolidArea().y - player.getSpeed()) / tileSize;
-      int bottomRow = (player.getY() + player.getSolidArea().y + player.getSolidArea().height
-          + player.getSpeed()) / tileSize;
-      int leftCol = (player.getX() + player.getSolidArea().x - player.getSpeed()) / tileSize;
-      int rightCol = (player.getX() + player.getSolidArea().x + player.getSolidArea().width
-          + player.getSpeed()) / tileSize;
+  private void checkForMapCollision() {
+    // the bounds of player's collision area
+    int topRow = (player.getY() + player.getSolidArea().y - player.getSpeed()) / tileSize;
+    int bottomRow = (player.getY() + player.getSolidArea().y + player.getSolidArea().height
+        + player.getSpeed()) / tileSize;
+    int leftCol = (player.getX() + player.getSolidArea().x - player.getSpeed()) / tileSize;
+    int rightCol = (player.getX() + player.getSolidArea().x + player.getSolidArea().width
+        + player.getSpeed()) / tileSize;
 
-      // check if the next move will collide with a certain tile
-      // out of bounds exception stops player from going off map
-      try {
-        player.setCollisionOn(
-            collision.checkTile(topRow, bottomRow, leftCol, rightCol, player.getDirection()));
-      } catch (ArrayIndexOutOfBoundsException e) {
-        player.setCollisionOn(true);
+    // check if the next move will collide with a certain tile
+    // out of bounds exception stops player from going off map
+    try {
+      player.setCollisionOn(
+          collision.checkTile(topRow, bottomRow, leftCol, rightCol, player.getDirection()));
+    } catch (ArrayIndexOutOfBoundsException e) {
+      player.setCollisionOn(true);
 
-      }
-    }
-
-  // checks if the map needs to change and places player in correct map and position
-  private void updateMap() {
-    int tileX = player.getX() / tileSize;
-    int tileY = player.getY() / tileSize;
-    Location current = gameMap.getCurrentLocation();
-    // checks if the tile is a transition tile and returns current map
-    String newMap = current.checkForMapTransition(tileX, tileY, player.getDirection());
-    if (!current.getName().equals(newMap)){
-      // change current location to new location
-      gameMap.setCurrentLocation(gameMap.getLocations().get(newMap));
-      // places the player at the entrance for the new map
-      int[] entrance = gameMap.getCurrentLocation().getEntranceCoordinates(player.getDirection());
-      player.setX(entrance[0] * tileSize);
-      player.setY(entrance[1] * tileSize);
-      // load the new map
-      tileManager.loadMap(gameMap.getCurrentLocation().getFilepath());
-      soundEffect.loadAndPlayFx("run");
     }
   }
 
-    public void pickupItem (String itemName){
-
-      if (!itemName.equals("") && (player.getInventory().getInventory().size() < 8)) {
-        System.out.println("Num of items: " + player.getInventory().getInventory().size());
-        try {
-          player.reduceStaminaPickUp();
-          // remove the item from the location inventory and add it to the player inventory
-          game.transferItemFromTo(inventory, player.getInventory(), itemName);
-          soundEffect.loadAndPlayFx("pickup");
-        } catch (InventoryEmptyException e) {
-          System.out.println("Item " + itemName + " is not in inventory!");
-        } catch (NoEnoughStaminaException e) {
-          //throw new RuntimeException(e);
-          System.out.println("Not enough stamina to pickup item!");
-        }
-      }
-    }
-    public void dropItem() {
-      int index = gameUI.getItemIndex();
-      Item foundItem = null;
-      for (Item item : game.getPlayerInventory().getInventory().values()) {
-        if (item.getIndex() == index) {
-          foundItem = item;
-          System.out.println("Index: " + index + " Name: " + item.getName());
-        }
-      }
-      if (foundItem != null) {
-        String action = foundItem.checkEatOrDrop();
-        if (action.equals("eat")) {
-          try {
-            game.eatItem(foundItem.getName());
-            soundEffect.loadAndPlayFx("eat");
-          } catch (InventoryEmptyException | ItemNotEdibleException |
-                   ConcurrentModificationException e) {
-            //System.out.println(e);
-          }
-        } else if (action.equals("drop")) {
-          try {
-            foundItem.updateItemLocation(player.getX(), player.getY());
-            game.transferItemFromTo(game.getPlayerInventory(), game.getCurrentLocationInventory(),
-                foundItem.getName());
-            soundEffect.loadAndPlayFx("drop");
-          } catch (InventoryEmptyException |
-                   ConcurrentModificationException |
-                   NoEnoughStaminaException e) {
-            //System.out.println(e);
-          }
-        }
-      }
-      keyHandler.setEnterPressed(false);
-    }
+  // checks if the map needs to change and places player in correct map and position
 
   public void playBackgroundMusic() {
     URL backgroundMusic = getClass().getResource(AllSounds.ALL_SOUNDS.get("main"));
     Music.runAudio(backgroundMusic);
-    //music.playBackgroundMusic();
   }
 
   public void stopBackgroundMusic() {
@@ -350,7 +287,7 @@ public class GamePanel extends JPanel implements Runnable {
     return tileSize;
   }
 
-  public Player getPlayer(){
+  public Player getPlayer() {
     return this.player;
   }
 
